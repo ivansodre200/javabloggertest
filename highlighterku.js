573a92a8580ae7da2165dd06353a0031d69ab5a1
ivansodre200 <89310211+ivansodre200@users.noreply.github.com>
2021-12-20 13:11:32 -0300

    Add files via upload
    navierdos2.blogspot.com
    Quero fazer um encurtador de url, esse e um test alpha

---
+++ highlighterku.js
@@ -0,0 +1,6 @@
+//<![CDATA[
+// Double Click
+$('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
+function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.rawgit.com/Indzign/theme/9cb6e56a/highlightprox.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+$(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
+//]]>
